d07cfbb204582d98fdc412c99346d1f3965ae61b
jlpdisc <95107841+jlpdisc@users.noreply.github.com>
2021-11-26 16:20:22 -0700

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import config from '../../config';
-import LogoImageMobile from './sneakertime-logo-mobile.png';
-import LogoImage from './sneakertime-logo.png';
+import LogoImageMobile from './discexchange-logo-mobile.png';
+import LogoImage from './discexchange-logo.png';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
