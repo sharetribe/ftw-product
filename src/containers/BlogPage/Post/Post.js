3ba02dead07bc3e44cb120ffcbdcfc377fb9d5b2
Harry Holcomb <hholcomb@smu.edu>
2022-11-06 18:17:56 -0600

    create route for blog posts

--- src/containers/BlogPage/Post/Post.js
+++ src/containers/BlogPage/Post/Post.js
@@ -5,14 +5,13 @@ import { AspectRatioWrapper, NamedLink, ResponsiveImage } from '../../../compone
 
 import "./Post.css";
 
-
 // First param should be title in named link
 const Post = ({ post: { title, body,
     imgUrl, author }, index }) => {
-        const slug = createSlug(title);
+      const slug = createSlug(title);
         const id = "6351ac74-462c-4f7b-9e93-14ad3809740c";
       return (  
-        <NamedLink className="post-container" name="BlogPage" params={{ id, slug }}> 
+        <NamedLink className="post-container" name="BlogPostPage" params={{ slug }}> 
           <h1 className="heading">{title}</h1>
           <img className="image" src={imgUrl} alt="post" />
           <p>{body}</p>
